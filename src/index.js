a975680282cff0ba9385f2d33131b896ac9fcf0c
oota <shark747474@gmail.com>
2019-07-02 23:02:22 +0900

    error correction

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-//console.log("aa");
-document.getElementById("nav").style.color = "blue"
 
 ReactDOM.render(<App />, document.getElementById('root'));
