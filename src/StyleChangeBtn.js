a35ccdb448575fb989474f08875e84b40586f28b
oota <shark747474@gmail.com>
2019-07-11 00:37:57 +0900

    add alert dialog

--- src/StyleChangeBtn.js
+++ src/StyleChangeBtn.js
@@ -3,13 +3,24 @@ import React from 'react';
 class StyleChangeBtn extends React.Component {
   constructor(props){
     super(props);
+    this.styleChange = this.styleChange.bind(this);
+  }
+
+  styleChange() {
+    if (this.props.styleURL == '') {
+      window.alert("スタイルを選択してください!!");
+    } else if (this.props.uploadedURL == '') {
+      window.alert("画像をアップロードしてください!!");
+    } else {
+      
+    }
   }
 
   render(){
     console.log(this.props.styleURL);
     console.log(this.props.uploadedURL);
     return(
-      <div className="button-item">
+      <div className="button-item" onClick={this.styleChange}>
         <p>スタイル変換</p>
       </div>
     );
